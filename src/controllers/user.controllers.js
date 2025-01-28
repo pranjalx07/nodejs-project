b93c4fafeacbe2a624ba4911d76b86d0d07d8eed
pranjalx07 <giripranzal588@gmail.com>
2025-01-28 22:46:12 +0545

    Corrected error handling

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,15 +1,34 @@
+import { userLoginService ,allUserService,WriteuserService} from "../services/userservices.js";
 
-import { userLoginService } from "../services/userservices.js";
-
-
-
-export const userLoginController=async(req,res)=>{
+export const userLoginController=async(req,res,next)=>{
     console.log(req);
     try{
     const data=await userLoginService(req.body);
     res.status(200).json({data})
     }catch(error){
         console.log(error);
+        next(error)
+    }
+}
+
+export const allUserController=async(req,res,next)=>{
+    console.log(req);
+    try{
+    const data=await allUserService(req.body);
+    res.status(200).json({data})
+    }catch(error){
+        console.log(error);
+        next(error)
+    }
+}
+
+export const writeUser=async(req,res,next)=>{
+    console.log(req)
+    try{
+        const data=await WriteuserService(req.body);
+        res.status(200).json({data})
+    }catch(error){
+        console.error(error)
         next(error)
     }
 }
